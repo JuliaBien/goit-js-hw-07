a25c86abad3541eb63e5bb4dcd9b537d1c5f4309
JuliaBien <150686648+JuliaBien@users.noreply.github.com>
2024-04-03 00:30:14 +0200

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+for (const item of galleryItems) {
+  const listElement = document.createElement('li');
+  listElement.innerHTML = `<a class="gallery__item" href=${item.original}>
+      <img
+        class="gallery__image"
+        src=${item.preview}
+        alt=${item.description}
+      />
+    </a>`;
+  galleryList.append(listElement);
+}
+const showOryginalSize = ev => {
+  ev.preventDefault();
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: 'true',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
+};
+galleryList.addEventListener('click', showOryginalSize);
