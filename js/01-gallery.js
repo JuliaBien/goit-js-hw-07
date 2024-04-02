c8729211d6bae6d366a37c2a91e06669bf21620a
JuliaBien <150686648+JuliaBien@users.noreply.github.com>
2024-04-02 15:48:35 +0200

    update

---
+++ js/01-gallery.js
@@ -0,0 +1,26 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryList = document.querySelector('.gallery');
+for (const item of galleryItems) {
+  const listElement = document.createElement('li');
+  listElement.className = 'gallery_item';
+  listElement.innerHTML = `<a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>`;
+  galleryList.append(listElement);
+}
+galleryList.addEventListener('click', showOryginalSize);
+const showOryginalSize = event => {
+  const instance = basicLightbox.create(`${event.target.getAttribute(
+    'data-source'
+  )}
+`);
+
+  instance.show();
+};
+console.log(galleryItems);
